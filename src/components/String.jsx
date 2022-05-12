dbecabd4bdf72b5bf78cb9d9487b8fa4d7ea91c6
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-05-11 23:16:41 -0400

    Implement mapStateToProps and mapDispatchToProps in String component.

--- src/components/String.jsx
+++ src/components/String.jsx
@@ -3,11 +3,23 @@ import { connect } from "react-redux";
 
 class String extends Component {
   state = {};
+
+  handleAddClick = () => {
+    let number = document.getElementById("number").value;
+    this.props.add(parseInt(number));
+  };
+  handleSubClick = () => {
+    let number = document.getElementById("number").value;
+    this.props.sub(parseInt(number));
+  };
   render() {
     return (
       <React.Fragment>
         <h3>String:</h3>
         <div>{this.props.string}</div>
+        <input type="number" id="number" />
+        <button onClick={this.handleAddClick}>Add Number</button>
+        <button onClick={this.handleSubClick}>Sub Number</button>
       </React.Fragment>
     );
   }
@@ -19,4 +31,19 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-export default connect(mapStateToProps)(String);
+const mapDispatchToProps = {
+  add: (number) => {
+    return {
+      type: "add",
+      value: number,
+    };
+  },
+  sub: (number) => {
+    return {
+      type: "sub",
+      value: number,
+    };
+  },
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(String);
